fa11a11e8656d1c18737141333a32ae74c165685
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-03-19 19:45:53 -0400

    Création et affichage d'un composant MyJob dans la page d'accueil

---
+++ src/components/HomeParts/MyJob/MyJob.jsx
@@ -0,0 +1,28 @@
+import style from "./MyJob.module.css"
+import FlexItem from "../../FlexItem/FlexItem"
+import Typewriter from 'typewriter-effect';
+import FeatherIcon from "feather-icons-react";
+
+
+export default function MyJob() {
+  return (
+    <FlexItem iconName="code">
+      <div className={style.appNameContainer}>
+        <div className={style.itemIcon}><FeatherIcon icon="terminal" size="20" stroke="white" strokeWidth={2.5} /></div>
+        <code className={style.appName}>C:\Users\yoleneconstable\Documents\my-portfolio&gt;</code>
+        <Typewriter
+          options={{
+            strings: "npm run dev",
+            autoStart: true,
+            cursor: "▮",
+            pauseFor: 300000
+          }}
+        />
+      </div>
+      <div className={style.jobInfo}>
+        <p className={style.jobName}>Développeuse front-end JavaScript</p>
+        <p className={style.location}>Basée entre <span className={style.locationBold}>Paris</span> et <span className={style.locationBold}>Montréal</span></p>
+      </div>
+    </FlexItem>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,18 @@ import style from "./Home.module.css"
 import Title from "../../components/HomeParts/Title/Title"
 import AboutMe from "../../components/HomeParts/AboutMe/AboutMe"
 import Resume from "../../components/HomeParts/Resume/Resume"
+import MyJob from "../../components/HomeParts/MyJob/MyJob"
 
 export default function Home() {
     return (
         <div className={style.container}>
             <div className={style.column}>
-                <Title/>
-                <AboutMe/>
-                <Resume/>
+                <Title />
+                <AboutMe />
+                <Resume />
+            </div>
+            <div className={style.column}>
+                <MyJob />
             </div>
         </div>
     )
