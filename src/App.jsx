17ccb0a742ba4ab43121ce11e4a010f55c27c88d
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-03-24 02:09:05 -0400

    Création et affichage de la page Contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import Navbar from "./components/Navbar/Navbar";
 import { HOME_ROUTE, PROJECTS_ROUTE, CONTACT_ROUTE, ABOUT_ROUTE } from "./config/routes";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   const location = useLocation();
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path={HOME_ROUTE} element={<Home />} />
         <Route path={PROJECTS_ROUTE} element={<Projects />} />
+        <Route path={CONTACT_ROUTE} element={<Contact />} />
       </Routes>
     </>
   )
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import style from "./Contact.module.css"
+import FeatherIcon from "feather-icons-react"
+import Map from "../../components/Map/Map"
+
+export default function Contact() {
+    return (
+        <div className={style.contactContainer}>
+            <div className={style.mapContainer}>
+                <Map />
+            </div>
+            <div className={style.contact}>
+                <div className={style.mailContainer}>
+                    <a href="mailto:yoleneconstable@gmail.com" className={style.mail}>
+                        <FeatherIcon icon="mail" size="20" fill="white" stroke="#1B1B1B" strokeWidth={1} />
+                        <span className={style.contactText}>yoleneconstable@gmail.com</span>
+                    </a>
+                    <a href="https://www.linkedin.com/in/constable-yolene/" target="_blank" className={style.linkedIn}>
+                        <FeatherIcon icon="linkedin" size="20" fill="white" stroke="transparent" />
+                        <span className={style.contactText}>LinkedIn</span>
+                    </a>
+                </div>
+                <a href="https://github.com/Yawleen" target="_blank" className={style.github}>
+                    <FeatherIcon icon="github" size="50" fill="white" stroke="transparent" />
+                </a>
+            </div>
+        </div>
+    )
+}
