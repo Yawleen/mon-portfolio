8f91a15e5036f07dbb61b4edfa868f22f1d3d249
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-04-06 21:22:50 -0400

    Modification du chemin du CV importé dans le composant Resume

--- src/components/HomeParts/Resume/Resume.jsx
+++ src/components/HomeParts/Resume/Resume.jsx
@@ -4,7 +4,7 @@ import FlexItem from "../../FlexItem/FlexItem"
 export default function Resume() {
     return (
         <FlexItem iconName="download" title="Mon CV" titleColor="#1B1B1B" bgColor="#ffffff79">
-            <a className={style.link} aria-label="Voir mon CV" href="/docs/CV_CONSTABLE_Yolène_Développeuse_Front-End.pdf" download />
+            <a className={style.link} aria-label="Voir mon CV" href={`${import.meta.env.BASE_URL}docs/CV_CONSTABLE_Yolène_Développeuse_Front-End.pdf`} download />
         </FlexItem>
     )
 }
