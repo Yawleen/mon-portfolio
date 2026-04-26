b1ff21ac1d19071c64e28c4f76145da2bde05213
Yolène <yoleneconstable@live.fr>
2026-04-26 21:53:34 +0200

    Mise à jour du texte dans le composant AboutMe

--- src/components/HomeParts/AboutMe/AboutMe.jsx
+++ src/components/HomeParts/AboutMe/AboutMe.jsx
@@ -1,14 +1,22 @@
-import style from "./AboutMe.module.css"
-import FlexItem from "../../FlexItem/FlexItem"
-import { ABOUT_ROUTE } from "../../../config/routes"
+import style from './AboutMe.module.css';
+import FlexItem from '../../FlexItem/FlexItem';
+import { ABOUT_ROUTE } from '../../../config/routes';
 
 export default function AboutMe() {
-    return (
-        <FlexItem grow title="Qui suis-je ?" link={{ label: "En savoir plus sur moi", href: ABOUT_ROUTE }}>
-            <p className={style.aboutMeText}>
-                Une aventurière avant tout ! J'ai exploré plusieurs chemins et je me suis beaucoup cherchée avant de trouver quelque chose qui me plaisait. Des sciences du Langage à l'IA pour enfin trouver ma place
-                dans le Développement Web. Depuis 3 ans maintenant, je m'épanouis en tant que Développeuse Front-End et je travaille principalement avec des frameworks JS comme React et Next.js. 🤗
-            </p>
-        </FlexItem>
-    )
+  return (
+    <FlexItem
+      grow
+      title="Qui suis-je ?"
+      link={{ label: 'En savoir plus sur moi', href: ABOUT_ROUTE }}
+    >
+      <p className={style.aboutMeText}>
+        Une aventurière avant tout ! J'ai exploré plusieurs chemins et je me
+        suis beaucoup cherchée avant de trouver quelque chose qui me plaisait.
+        Des sciences du Langage à l'IA pour enfin trouver ma place dans le
+        Développement Web. Depuis 4 ans maintenant, je m'épanouis en tant que
+        Développeuse Front-End et je travaille principalement avec des
+        frameworks JS comme React et Next.js. 🤗
+      </p>
+    </FlexItem>
+  );
 }
