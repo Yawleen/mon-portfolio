429c5aee6a0adb819225c5b9edbd15dc51df97bc
Yolène <yoleneconstable@live.fr>
2026-04-26 21:57:28 +0200

    Mise à jour du texte dans le composant MyJob

--- src/components/HomeParts/MyJob/MyJob.jsx
+++ src/components/HomeParts/MyJob/MyJob.jsx
@@ -1,27 +1,35 @@
-import style from "./MyJob.module.css"
-import FlexItem from "../../FlexItem/FlexItem"
+import style from './MyJob.module.css';
+import FlexItem from '../../FlexItem/FlexItem';
 import Typewriter from 'typewriter-effect';
-import FeatherIcon from "feather-icons-react";
-
+import FeatherIcon from 'feather-icons-react';
 
 export default function MyJob() {
   return (
     <FlexItem iconName="code">
       <div className={style.appNameContainer}>
-        <div className={style.itemIcon}><FeatherIcon icon="terminal" size="20" stroke="white" strokeWidth={2.5} /></div>
-        <code className={style.appName}>C:\Users\yoleneconstable\Documents\my-portfolio&gt;</code>
+        <div className={style.itemIcon}>
+          <FeatherIcon
+            icon="terminal"
+            size="20"
+            stroke="white"
+            strokeWidth={2.5}
+          />
+        </div>
+        <code className={style.appName}>
+          ~/Desktop/Dev/Web/React/mon-portfolio
+        </code>
         <Typewriter
           options={{
-            strings: "npm run dev",
+            strings: 'npm run dev',
             autoStart: true,
-            cursor: "▮",
-            pauseFor: 300000
+            cursor: '▮',
+            pauseFor: 300000,
           }}
         />
       </div>
       <div className={style.jobInfo}>
-        <p className={style.jobName}>Développeuse Front-End JavaScript</p>
+        <p className={style.jobName}>Développeuse Front-End</p>
       </div>
     </FlexItem>
-  )
+  );
 }
