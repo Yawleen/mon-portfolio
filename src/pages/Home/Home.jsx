8379de98b79fc70540a1fe736a939fb74d529d3c
Yolène <yoleneconstable@live.fr>
2026-04-26 21:51:57 +0200

    Suppression du composant MyCertifications de la page d'accueil

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,43 +1,41 @@
-import style from "./Home.module.css"
-import Title from "../../components/HomeParts/Title/Title"
-import AboutMe from "../../components/HomeParts/AboutMe/AboutMe"
-import Resume from "../../components/HomeParts/Resume/Resume"
-import MyJob from "../../components/HomeParts/MyJob/MyJob"
-import MyAvatar from "../../components/HomeParts/MyAvatar/MyAvatar"
-import ContactMe from "../../components/HomeParts/ContactMe/ContactMe"
-import MyProjects from "../../components/HomeParts/MyProjects/MyProjects"
-import MyTech from "../../components/HomeParts/MyTech/MyTech"
-import MyExperiences from "../../components/HomeParts/MyExperiences/MyExperiences"
-import MyEducation from "../../components/HomeParts/MyEducation/MyEducation"
-import MyCertifications from "../../components/HomeParts/MyCertifications/MyCertifications"
-import { useEffect } from "react"
+import style from './Home.module.css';
+import Title from '../../components/HomeParts/Title/Title';
+import AboutMe from '../../components/HomeParts/AboutMe/AboutMe';
+import Resume from '../../components/HomeParts/Resume/Resume';
+import MyJob from '../../components/HomeParts/MyJob/MyJob';
+import MyAvatar from '../../components/HomeParts/MyAvatar/MyAvatar';
+import ContactMe from '../../components/HomeParts/ContactMe/ContactMe';
+import MyProjects from '../../components/HomeParts/MyProjects/MyProjects';
+import MyTech from '../../components/HomeParts/MyTech/MyTech';
+import MyExperiences from '../../components/HomeParts/MyExperiences/MyExperiences';
+import MyEducation from '../../components/HomeParts/MyEducation/MyEducation';
+import { useEffect } from 'react';
 
 export default function Home() {
-    useEffect(() => {
-        document.title = "Yolène Dev Front-End | Portfolio";
-    }, []);
+  useEffect(() => {
+    document.title = 'Yolène Dev Front-End | Portfolio';
+  }, []);
 
-    return (
-        <div className={style.container}>
-            <div className={style.column}>
-                <Title />
-                <AboutMe />
-                <Resume />
-            </div>
-            <div className={style.column}>
-                <MyJob />
-                <MyAvatar />
-                <ContactMe />
-            </div>
-            <div className={style.column}>
-                <MyProjects />
-                <MyTech />
-            </div>
-            <div className={style.column}>
-                <MyExperiences />
-                <MyEducation />
-                <MyCertifications />
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.container}>
+      <div className={style.column}>
+        <Title />
+        <AboutMe />
+        <Resume />
+      </div>
+      <div className={style.column}>
+        <MyJob />
+        <MyAvatar />
+        <ContactMe />
+      </div>
+      <div className={style.column}>
+        <MyProjects />
+        <MyTech />
+      </div>
+      <div className={style.column}>
+        <MyExperiences />
+        <MyEducation />
+      </div>
+    </div>
+  );
 }
