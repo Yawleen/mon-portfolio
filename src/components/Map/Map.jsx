b6fdf6504db110d8ee2a82d8ad1e546063017158
Yolène <yoleneconstable@live.fr>
2026-04-26 22:29:13 +0200

    Mise à jour des coordonnées géographiques dans le composant Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,38 +1,40 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import style from "./Map.module.css"
-import L from "leaflet";
-import pin from "../../assets/img/yolène_pin.webp"
-import FeatherIcon from "feather-icons-react";
-
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import style from './Map.module.css';
+import L from 'leaflet';
+import pin from '../../assets/img/yolène_pin.webp';
+import FeatherIcon from 'feather-icons-react';
 
 const customIcon = new L.Icon({
-    iconUrl: pin,
-    iconSize: [80, 80],
+  iconUrl: pin,
+  iconSize: [70, 70],
 });
 
 export default function Map() {
-    const position = [49.2661504, 1.8915763];
+  const position = [48.9609401, 1.8028323];
 
-    return (
-        <MapContainer
-            center={position}
-            zoom={5}
-            className={style.map}
-            scrollWheelZoom={false}
-            zoomControl={false}
-            doubleClickZoom={false}
-            touchZoom={false}
-        >
-            <TileLayer
-                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            />
-            <Marker position={position} icon={customIcon}/>
-            <div className={style.cityContainer}>
-                <FeatherIcon icon="map-pin" size="15" fill="#f44336" stroke="white" strokeWidth={2} />
-                <p className={style.city}>Chaumont-en-Vexin, France</p>
-            </div>
-        </MapContainer>
-    );
-};
-
+  return (
+    <MapContainer
+      center={position}
+      zoom={5}
+      className={style.map}
+      scrollWheelZoom={false}
+      zoomControl={false}
+      doubleClickZoom={false}
+      touchZoom={false}
+    >
+      <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
+      <Marker position={position} icon={customIcon} />
+      <div className={style.cityContainer}>
+        <FeatherIcon
+          icon="map-pin"
+          size="15"
+          fill="#f44336"
+          stroke="white"
+          strokeWidth={2}
+        />
+        <p className={style.city}>Aubergenville, France</p>
+      </div>
+    </MapContainer>
+  );
+}
