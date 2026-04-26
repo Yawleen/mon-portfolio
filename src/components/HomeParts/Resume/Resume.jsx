7bb1caecf6b4da313a699f2748f5953c27588f59
Yolène <yoleneconstable@live.fr>
2026-04-26 23:34:57 +0200

    Mise à jour du CV et du composant Resume

--- src/components/HomeParts/Resume/Resume.jsx
+++ src/components/HomeParts/Resume/Resume.jsx
@@ -1,10 +1,20 @@
-import style from "./Resume.module.css"
-import FlexItem from "../../FlexItem/FlexItem"
+import style from './Resume.module.css';
+import FlexItem from '../../FlexItem/FlexItem';
 
 export default function Resume() {
-    return (
-        <FlexItem iconName="download" title="Mon CV" titleColor="#1B1B1B" bgColor="#ffffff79">
-            <a className={style.link} aria-label="Voir mon CV" href={`${import.meta.env.BASE_URL}docs/CV_CONSTABLE_Yolène_Développeuse_Front-End.pdf`} download />
-        </FlexItem>
-    )
+  return (
+    <FlexItem
+      iconName="download"
+      title="Mon CV"
+      titleColor="#1B1B1B"
+      bgColor="#ffffff79"
+    >
+      <a
+        className={style.link}
+        aria-label="Voir mon CV"
+        href={`${import.meta.env.BASE_URL}docs/cv_yolene_constable_dev_frontend.pdf`}
+        download
+      />
+    </FlexItem>
+  );
 }
